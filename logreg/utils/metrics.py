c9d07bfaa0f1ff903d6ab32a5472c6c6697aa8d5
Artur Yakupov <art.kazan@mail.ru>
2023-04-04 02:25:51 +0300

    completed logreg: 1. training 2. normal, chi, uniform, xavier weights init 3. saving 4. plots

---
+++ logreg/utils/metrics.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def MSE(predictions: np.ndarray, targets: np.ndarray) -> float:
+    pass
+
+
+def accuracy(predictions: np.ndarray, targets: np.ndarray) -> float:
+    return sum(map(lambda x: 1 if x[0] == x[1] else 0, zip(predictions, targets))) / targets.size
+
+def confusion_matrix(classes_number: int, predicted_classes: np.ndarray, targets: np.ndarray):
+    # Ox predicted Oy granted truth
+    matrix = np.zeros((classes_number, classes_number))
+    for i in range(predicted_classes.size):
+        x = predicted_classes[i]
+        y = targets[i]
+        matrix[y, x] += 1
+    return matrix
